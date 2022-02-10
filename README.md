9ca28139f2f329fab5610d5143317a766cfb4a08
shermansplanet <lsherman@mit.edu>
2022-02-10 11:11:48 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-k5fuue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-k5fuue)
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css';
+
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: 'AIzaSyBaO_U5bGJbdxprRhGuiifxP8GZTBA4Avc',
+  authDomain: 'practiles.firebaseapp.com',
+  databaseURL: 'https://practiles-default-rtdb.firebaseio.com',
+  projectId: 'practiles',
+  storageBucket: 'practiles.appspot.com',
+  messagingSenderId: '351914796197',
+  appId: '1:351914796197:web:61714e3cde8e22ee43594c',
+};
+const app = initializeApp(firebaseConfig);
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
