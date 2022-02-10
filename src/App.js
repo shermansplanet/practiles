83666e7b1cdc2b67f609581c3efe24795a1ec0a0
shermansplanet <lsherman@mit.edu>
2022-02-10 11:17:52 -0800

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './style.css';
 
 import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, onValue } from 'firebase/database';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyBaO_U5bGJbdxprRhGuiifxP8GZTBA4Avc',
   authDomain: 'practiles.firebaseapp.com',
@@ -12,6 +14,13 @@ const firebaseConfig = {
   appId: '1:351914796197:web:61714e3cde8e22ee43594c',
 };
 const app = initializeApp(firebaseConfig);
+
+const db = getDatabase(app);
+const dbref = ref(db, '/');
+onValue(dbref, (snapshot) => {
+  const data = snapshot.val();
+  console.log(data);
+});
 
 export default function App() {
   return (
